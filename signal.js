010459b7d9fae36807615f8a6bae7c0b4fed9cdd
shu-baiqiao <shu.baiqiao@navercorp.com>
2021-01-13 16:48:54 +0800

    Feat: Complete the signal file.

--- signal.js
+++ signal.js
@@ -21,11 +21,59 @@ class Signal {
   }
 
   set state(value) {
-    [...this[_checkers]].forEach(({type, deferred, state}) => {
+    [...this[_checkers]].forEach(([promise, {type, deferred, state}]) => {
       if (type === 'while' && value !== state || type === 'until' && value === state) {
         deferred.resolve(value);
         this[_checkers].delete(promise);
       }
     })
+    this[_state] = value
+  }
+
+  until(state) {
+    const deferred = defer();
+    if (state === this[_state]) {
+      deferred.resolve(this[_state])
+    } else {
+      this[_checkers].set(deferred.promise, { type: 'until', deferred, state})
+    }
+    return deferred.promise;
+  }
+
+  while(state) {
+    const deferred = defer();
+    if (state !== this[_state]) {
+      deferred.resolve(this[_state])
+    } else {
+      this[_checkers].set(deferred.promise, { type: 'while', deferred, state})
+    }
+    return deferred.promise;
+  }
+
+  delete(promise) {
+    this[_checkers].delete(promise);
+  }
+
+  deleteAll() {
+    this[_checkers].clear();
   }
 }
+
+const lucky = new Signal(0);
+
+const timerID = setInterval(() => {
+  const num = Math.ceil(Math.random() * 10);
+  console.log(num);
+  lucky.state = num;
+}, 1000);
+
+async function addLuckyBoy(name, num) {
+  await lucky.until(num);
+  console.log(`${name} is lucky boy!`);
+  clearInterval(timerID);
+  lucky.deleteAll(); // 删除checkers中的所有promise对象
+}
+
+addLuckyBoy('张三', 3);
+addLuckyBoy('李四', 5);
+addLuckyBoy('王五', 7);
