dcbbb8324a91d829378ede9fcc91474bf5cc34a0
shu-baiqiao <shu.baiqiao@navercorp.com>
2021-03-31 10:06:40 +0800

    Feat: Add the subsetsWithDup solution.

---
+++ subsetsWithDup.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  if (!nums.length) return [[]];
+  nums.sort((a, b) => a - b);
+  const formalArr = nums.slice(0, -1);
+  const upperResult = subsetsWithDup(formalArr);
+  return [
+    ...upperResult.filter(
+      (item) => item[item.length - 1] !== nums[nums.length - 1]
+    ),
+    ...upperResult.map((item) => {
+      return [...item, nums[nums.length - 1]];
+    }),
+  ];
+};
+
+console.log(subsetsWithDup([1, 1, 2, 2, 3]));
+
+module.exports = subsetsWithDup;
